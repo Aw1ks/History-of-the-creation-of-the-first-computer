21934b2dc657fe55be20a6e28654340c3bb45abb
Aw1ks <145365323+Aw1ks@users.noreply.github.com>
2025-04-16 20:01:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import pdfplumber
+from gtts import gTTS
+
+
+def get_pdf_text(pdf_file_path):
+    with pdfplumber.open(pdf_file_path) as pdf:
+        pages_text = ''.join([page.extract_text() for page in pdf.pages])
+        pages_text_cleaned = pages_text.replace("\n", "")
+        return pages_text_cleaned
+
+
+def create_pdf_audio(pdf_text, language='ru', output_file='output.mp3'):
+    audio = gTTS(text=pdf_text, lang=language)
+    audio.save(output_file)
+
+
+def main():
+    pdf_file_path = 'text.pdf'
+    pdf_text = get_pdf_text(pdf_file_path)
+    create_pdf_audio(pdf_text)
+
+
+if __name__ == '__main__':
+    main()
